616b942677838405e0f4267bfd621285210dfa44
Ethan Winn <ethan@colab.coop>
2014-09-19 16:09:16 -0400

    removed sunrise commander

--- personal/personal-sunrise-commander.el
+++
@@ -1,9 +0,0 @@
-(setq package-archives (cons
-                        '("SC"   . "http://joseito.republika.pl/sunrise-commander/")
-                        package-archives
-                        )
-      )
-
-(prelude-require-packages '(sunrise-commander))
-
-(provide 'personal-sunrise-commander)
